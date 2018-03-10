02526c895bef8c4c4b57926fa64f20edc397c3a3
de Freitas Neto <f.de.freitas.neto@accenture.com>
2018-03-10 09:44:22 -0300

    rotas para leitura de enviroment viriables

--- app.js
+++ app.js
@@ -2,12 +2,20 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/', function(req, res)
-{
+app.get('/', function (req, res) {
     res.send('Hello World!');
 })
 
-app.listen(PORT, function()
-{
+app.get('/env', function (req, res) {
+    res.contentType('application/json');
+    res.send(process.env);
+})
+
+app.get('/env/:param/', function (req, res) {
+    res.contentType('application/json');
+    res.send(process.env[req.params.param]);
+})
+
+app.listen(PORT, function () {
     console.log('NodeJS HelloWorld app listening to port ...' + PORT);
 })
